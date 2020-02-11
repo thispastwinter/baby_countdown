9ac6cc4ff791391dd15753c17203d4e113d2f14f
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2020-02-10 20:59:18 -0600

    Initial commit

---
+++ _templates/util/new/module.ejs.t
@@ -0,0 +1,10 @@
+---
+to: src/utils/<%= name %>/index.ts
+---
+/**
+ * <%= name %>
+ * ------------------------------------------------
+ * DESCRIPTION_HERE
+ */
+
+export const <%= name %> = <A>(x: A) => x
---
+++ src/components/App/test.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { App } from "."
+
+describe("App", () => {
+  it("renders", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
