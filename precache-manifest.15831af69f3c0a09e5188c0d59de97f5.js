d0223e3b4caeb7cbeeff89c0365fa6a1a0b87e08
thispastwinter <klaas.justin@gmail.com>
2020-02-11 08:17:39 -0600

    Updates

---
+++ precache-manifest.15831af69f3c0a09e5188c0d59de97f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b7f1941e851c491770334f09222df2",
+    "url": "/baby_countdown/index.html"
+  },
+  {
+    "revision": "656da36c52dce74ab36f",
+    "url": "/baby_countdown/static/css/main.5ff5e5c4.chunk.css"
+  },
+  {
+    "revision": "d0be48961453afab6070",
+    "url": "/baby_countdown/static/js/2.b8be2a57.chunk.js"
+  },
+  {
+    "revision": "32da2c8235d3abfdac0ab18d498a59e0",
+    "url": "/baby_countdown/static/js/2.b8be2a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656da36c52dce74ab36f",
+    "url": "/baby_countdown/static/js/main.e84be060.chunk.js"
+  },
+  {
+    "revision": "a810163bc26feb42acc6",
+    "url": "/baby_countdown/static/js/runtime-main.021d3432.js"
+  },
+  {
+    "revision": "1f8e5c69fd0e9a827f48b8336dc419ff",
+    "url": "/baby_countdown/static/media/rubber-duck.1f8e5c69.svg"
+  }
+]);
